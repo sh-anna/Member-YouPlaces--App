b4d26a7144da3a686d9fb38c0148dac01bb11b52
sh-anna <sharipkinanna@gmail.com>
2021-01-26 21:29:04 +0200

    adding header and navbar components

--- frontend/src/user/components/userList/usersList.js
+++ frontend/src/user/components/userList/usersList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './userList.css';
-import UserItem from '../userItem/userItem'
+import UserItem from '../userItem/userItem';
+import Card from '../../../shared/components/uiElements/card/card';
 
 const UserList = props => {
     if (props.items.lenght === 0) {
         return (
             <div className="center">
-                <h2>No users found.</h2>
+                <Card>
+                    <h2>No users found.</h2>
+                </Card>
             </div>
         );
     }
