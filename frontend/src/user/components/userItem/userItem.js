8f3acf53122f495aa47aa3ceabf365fd28a4d50c
sh-anna <sharipkinanna@gmail.com>
2021-01-26 16:27:16 +0200

    adding more components and styling

--- frontend/src/user/components/userItem/userItem.js
+++ frontend/src/user/components/userItem/userItem.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './userItem.css';
-
+import Avatar from "../../../shared/components/uiElements/avatar/avatar"
+import Card from "../../../shared/components/uiElements/card/card"
 
 const UserItem = props => {
     return(
         <li className="users-item">
-            <div className="user-item_content">
-                <div className="user-item_image">
-                    <img srs={props.image} alt={props.name}  />
-                </div>
-                <div className="user-item_info">
-                    <h2>{props.name}</h2>
-                    <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
-                </div>
-            </div>
+            <Card>
+                <Link to={`/${props.id}/places`}>
+                    <div className="user-item_image">
+                        <Avatar image={props.image} alt={props.name}  />
+                    </div>
+                    <div className="user-item_info">
+                        <h2>{props.name}</h2>
+                        <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
+                    </div>
+                </Link>
+            </Card>
         </li>
         );
 };
