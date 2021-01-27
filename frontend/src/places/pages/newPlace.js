8a046cbb9a2b4b1d1ab791155c9f4c5a7703ed5a
sh-anna <sharipkinanna@gmail.com>
2021-01-27 21:26:19 +0200

    Added front and back ends

---
+++ frontend/src/places/pages/newPlace.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewPlace = () => {
+    
+    return <h2>NewPlace work!</h2>
+    
+};
+
+export default NewPlace;
---
+++ frontend/src/shared/components/uiElements/card/card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Link } from "react-router-dom";
+// import Avatar from "../avatar/avatar";
+import "../../../../user/components/userItem/userItem.css";
+import './card.css';
+
+const Card = props => {
+  return (
+    <div className={`card ${props.className}`} style={props.style}>
+      {props.children}
+    </div>
+  );
+};
+
+export default Card;
