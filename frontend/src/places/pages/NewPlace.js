9580142520a5ae1c62589f061e0905befbbe6b50
sh-anna <sharipkinanna@gmail.com>
2021-01-29 09:39:14 +0200

    input component

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import Input from '../../shared/components/formElements/input/input';
+import './newPlacec.css';
 const NewPlace = () => {
     
-    return <h2>NewPlace work!</h2>
+    return <form className="plase-form">
+        <Input element="input" type="text" label="title" validators={[]}
+        errorText="Please enter a valid title."/>
+
+    </form>;
     
 };
 
