5c0fec127dc4f3d9e1b9b6f0d9caf7e10e613257
sh-anna <sharipkinanna@gmail.com>
2021-01-30 15:27:37 +0200

    show deleting warning

--- frontend/src/places/components/placeItem/placeItem.js
+++ frontend/src/places/components/placeItem/placeItem.js
@@ -6,45 +6,84 @@ import Map from '../../../shared/components/uiElements/map/map';
 import './placeItem.css';
 
 const PlaceItem = props => {
-    const [showMap, setShowMap] = useState(false);
-    const openMapHandler = () => setShowMap(true);
-    const closeMapHandler = () => setShowMap(false);
-    return (
-      <React.Fragment>
-        <Modal
-          show={showMap}
-          onCancel={closeMapHandler}
-          header={props.address}
-          contentClass="place-item__modal-content"
-          footerClass="place-item__modal-actions"
-          footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
-        >
-          <div className="map-container">
-            <Map  center={props.coordinates} zoom={16}/>
+  const [showMap, setShowMap] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+
+  const closeMapHandler = () => setShowMap(false);
+
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true);
+  };
+
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false);
+  };
+
+  const confirmDeleteHandler = () => {
+    setShowConfirmModal(false);
+    console.log('DELETING...');
+  };
+
+  return (
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <Map center={props.coordinates} zoom={16} />
+        </div>
+      </Modal>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="place-item__modal-actions"
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>
+              CANCEL
+            </Button>
+            <Button danger onClick={confirmDeleteHandler}>
+              DELETE
+            </Button>
+          </React.Fragment>
+        }
+      >
+        <p>
+          Do you want to proceed and delete this place? Please note that it
+          can't be undone thereafter.
+        </p>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title} />
           </div>
-        </Modal>
-        <li className="place-item">
-          <Card className="place-item__content">
-            <div className="place-item__image">
-              <img src={props.image} alt={props.title} />
-            </div>
-            <div className="place-item__info">
-              <h2>{props.title}</h2>
-              <h3>{props.address}</h3>
-              <p>{props.description}</p>
-            </div>
-            <div className="place-item__actions">
-              <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-              <Button to={`/places/${props.id}`}>EDIT</Button>
-              <Button danger>DELETE</Button>
-            </div>
-          </Card>
-        </li>
-      </React.Fragment>
-    );
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
+            <Button to={`/places/${props.id}`}>EDIT</Button>
+            <Button danger onClick={showDeleteWarningHandler}>
+              DELETE
+            </Button>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
+  );
 };
-  
+
 export default PlaceItem;
-
-
-  
