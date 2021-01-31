b187692c73bca145eb09c0d9681309bb8cdbf106
sh-anna <sharipkinanna@gmail.com>
2021-01-31 15:46:21 +0200

    add places routing

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const placesRoutes = require('./routes/places-routes');
+const app = express();
+
+app.use(placesRoutes);
+app.listen(8080);
+
