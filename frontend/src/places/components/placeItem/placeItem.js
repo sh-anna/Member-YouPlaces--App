7641e216178553de2df680fc799e7b5d4a5247f7
sh-anna <sharipkinanna@gmail.com>
2021-01-31 13:58:13 +0200

    logout button

--- frontend/src/places/components/placeItem/placeItem.js
+++ frontend/src/places/components/placeItem/placeItem.js
@@ -1,11 +1,13 @@
-import React , {useState } from 'react';
+import React , {useState, useContext } from 'react';
 import Card from '../../../shared/components/uiElements/card/card';
 import Button from '../../../shared/components/formElements//button';
 import Modal from '../../../shared/components/uiElements/modal/modal';
 import Map from '../../../shared/components/uiElements/map/map';
+import { AuthContext } from '../../../shared/context/auth-context';
 import './placeItem.css';
 
 const PlaceItem = props => {
+  const auth = useContext(AuthContext);
   const [showMap, setShowMap] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
 
@@ -75,10 +77,15 @@ const PlaceItem = props => {
             <Button inverse onClick={openMapHandler}>
               VIEW ON MAP
             </Button>
-            <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger onClick={showDeleteWarningHandler}>
-              DELETE
-            </Button>
+            {auth.isLoggedIn && (
+              <Button to={`/places/${props.id}`}>EDIT</Button>
+            )}
+
+            {auth.isLoggedIn && (
+              <Button danger onClick={showDeleteWarningHandler}>
+                DELETE
+              </Button>
+            )}
           </div>
         </Card>
       </li>
