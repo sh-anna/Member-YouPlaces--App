c0c7a59ca2b9c6732631707f994632b65f35bbf2
sh-anna <sharipkinanna@gmail.com>
2021-01-31 17:39:33 +0200

    add error model

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const HttpError = require('../models/http.error');
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -25,9 +25,7 @@ router.get('/:pid', (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error('Could not find a place for the provided id.');
-    error.code = 404;
-    throw error;
+    throw new HttpError('Could not find a place for the provided id.', 404);
   }
 
   res.json({ place }); // => { place } => { place: place }
@@ -41,9 +39,9 @@ router.get('/user/:uid', (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error('Could not find a place for the provided user id.');
-    error.code = 404;
-    return next(error);
+    return next(
+      new HttpError('Could not find a place for the provided user id.', 404)
+    );
   }
 
   res.json({ place });
