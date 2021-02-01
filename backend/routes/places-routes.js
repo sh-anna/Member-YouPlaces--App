9f5b346ace2b06f0414d20c3a79fc3de430d3d58
sh-anna <sharipkinanna@gmail.com>
2021-02-01 09:36:53 +0200

    and the get places by user ID

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -6,12 +6,12 @@ const router = express.Router();
 
 router.get('/:pid', placesControllers.getPlaceById);
 
-router.get('/user/:uid', placesControllers.getPlaceByUserId);
+router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
 router.post('/', placesControllers.createPlace);
 
-router.patch('/:pid', );
+router.patch('/:pid', placesControllers.updatePlace);
 
-router.delete('/:pid', );
+router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports = router;
