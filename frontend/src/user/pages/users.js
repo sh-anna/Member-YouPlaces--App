35e702dce2b2371ac362944855a4858d6a023672
sh-anna <sharipkinanna@gmail.com>
2021-02-04 18:14:58 +0200

    using http hook to get users

--- frontend/src/user/pages/users.js
+++ frontend/src/user/pages/users.js
@@ -2,50 +2,36 @@ import React, { useEffect, useState } from 'react';
 import UsersList from '../components/userList/usersList'
 import ErrorModal from '../../shared/components/uiElements/errorModal/errorModal';
 import LoadingSpinner from '../../shared/components/uiElements/loadingSpinner/loadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 const Users = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState();
-    const [loadedUsers, setLoadedUsers] = useState();
-  
-    useEffect(() => {
-      const sendRequest = async () => {
-        setIsLoading(true);
-        try {
-          const response = await fetch('http://localhost:8080/api/users');
-  
-          const responseData = await response.json();
-  
-          if (!response.ok) {
-            throw new Error(responseData.message);
-          }
-  
-          setLoadedUsers(responseData.users);
-        } catch (err) {
-          setError(err.message);
-        }
-        setIsLoading(false);
-      };
-      sendRequest();
-    }, []);
-  
-    const errorHandler = () => {
-      setError(null);
-    };
-  
-    return (
-      <React.Fragment>
-        <ErrorModal error={error} onClear={errorHandler} />
-        {isLoading && (
-          <div className="center">
-            <LoadingSpinner />
-          </div>
-        )}
-        {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
-      </React.Fragment>
-    );
-  };
-  
-  export default Users;
-  
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedUsers, setLoadedUsers] = useState();
 
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const responseData = await sendRequest(
+          'http://localhost:8080/api/users'
+        );
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {}
+    };
+    fetchUsers();
+  }, [sendRequest]);
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
+};
+
+export default Users;
