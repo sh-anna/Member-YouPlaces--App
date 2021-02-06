3f2655f0cc462afcaf7bcb028b93b8c0dd19876e
sh-anna <sharipkinanna@gmail.com>
2021-02-06 18:28:05 +0200

    delete image when the plase bin delate

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,4 +1,5 @@
-const uuid = require('uuid/v4');
+// const uuid = require('uuid/v4');
+const fs = require('fs');
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 const HttpError = require('../models/http-error');
@@ -179,6 +180,8 @@ const deletePlace = async (req, res, next) => {
     return next(error);
   }
 
+  const imagePath = place.image;
+
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
@@ -193,6 +196,10 @@ const deletePlace = async (req, res, next) => {
     );
     return next(error);
   }
+
+  fs.unlink(imagePath, err => {
+    console.log(err);
+  });
 
   res.status(200).json({ message: 'Deleted place.' });
 };
