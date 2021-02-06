c1a3f14640afef17fc260be98baab9ac4f42d85c
sh-anna <sharipkinanna@gmail.com>
2021-02-06 09:57:05 +0200

    loading plasec by user id

--- frontend/src/places/pages/userPlaces.js
+++ frontend/src/places/pages/userPlaces.js
@@ -1,40 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import PlaceList from '../../places/components/placeList/placeList';
-import './newPlacec.css';
+import ErrorModal from '../../shared/components/uiElements/errorModal//errorModal';
+import LoadingSpinner from '../../shared/components/uiElements/loadingSpinner/loadingSpinner';
+import {useHttpClient} from '../../shared/hooks/http-hook';
 
 
-const DUMMY_PLACES = [
-        {
-          id: 'p1',
-          title: 'Yaffa',
-          description: 'The southern and oldest part of Tel Aviv-Yafo, is an ancient port city in Israel.',
-          imageUrl: 'https://www.namalyafo.co.il/wp-content/uploads/2019/02/DJI_0836doronsaar-e1574798133810.jpg?x87381',
-          address: 'nowadays has turned into one of the quarters of Tel Aviv',
-          location: {
-            lat: 32.109333,
-            lng: 34.855499
-          },
-          creator: 'u1'
-        },
-        {
-            id: 'p2',
-            title: 'Yaffa',
-            description: 'The southern and oldest part of Tel Aviv-Yafo, is an ancient port city in Israel.',
-            imageUrl: 'https://www.namalyafo.co.il/wp-content/uploads/2019/02/DJI_0836doronsaar-e1574798133810.jpg?x87381',
-            address: 'nowadays has turned into one of the quarters of Tel Aviv',
-            location: {
-              lat: 32.109333,
-              lng: 34.855499
-            },
-            creator: 'u2'
-        }
-      ];
-      
-    const UserPlaces = () => {
-        const userId = useParams().userId;
-        const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
-        return <PlaceList items={loadedPlaces} />;
+
+const UserPlaces = () => {
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  const userId = useParams().userId;
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:8080/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
     };
-      
-      export default UserPlaces;  
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </React.Fragment>
+  );
+};
+
+export default UserPlaces; 
