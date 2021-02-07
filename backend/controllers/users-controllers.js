e6d4f1cb070d99f06d41dc0124b495573f57d97b
sh-anna <sharipkinanna@gmail.com>
2021-02-07 09:39:36 +0200

    login user with hashed password

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -49,20 +49,20 @@ const signup = async (req, res, next) => {
   let hashedPassword;
   try {
     hashedPassword = await bcrypt.hash(password, 12);
-   } catch(err) {
-     const error = new HttpError('Could not create user, please try again.', 
-     500
-     );
-     return next(error);
-   }
-  
+  } catch (err) {
+    const error = new HttpError(
+      'Could not create user, please try again.',
+      500
+    );
+    return next(error);
+  }
 
   const createdUser = new User({
     name,
     email,
     image: req.file.path,
     password: hashedPassword,
-    places: [],
+    places: []
   });
 
   try {
@@ -93,7 +93,26 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
+      401
+    );
+    return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      'Could not log you in, please check your credentials and try again.',
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
     const error = new HttpError(
       'Invalid credentials, could not log you in.',
       401
