d3711b007158939f35bbfa795a391c4055de8714
sh-anna <sharipkinanna@gmail.com>
2021-02-09 11:09:31 +0200

    social-icons in footer

--- frontend/src/shared/components/navigation/footer/footer.js
+++ frontend/src/shared/components/navigation/footer/footer.js
@@ -1,32 +1,22 @@
 import React from 'react';
-// import { SocialIcon } from 'react-native-elements';
+
+import { SocialIcon } from 'react-social-icons';
 import './footer.css';
 
 
 const Footer = props => {
     
     return <footer className="footer">
-            <ul>
-{/*                 
-                    <SocialIcon to='https://www.twitter.com' title='Sign In With Twitter'
-                    button type='twitter'/>
-                
-                
-                    <SocialIcon to='https://www.facebook.com' title='Sign In With Facebook'
-                    button type='facebook'/>
-                
-                
-                    <SocialIcon to='https://www.instagram.com' title='Sign In With Instagram'
-                    button type='instagram'/>
-                
-                
-                    <SocialIcon title='Sign In With GitHub'
-                    button type='github'/> */}
-               
-            
-            
+            <ul className="social-icon">
+                <SocialIcon url="http://facebook.com/" bgColor="#3B5998" fgColor="#FFFFFF" style={{ height: 40, width: 40 }}/>
+                <SocialIcon url="http://twitter.com/" bgColor="#1DA1F2" fgColor="#FFFFFF" style={{ height: 40, width: 40 }}/>
+                <SocialIcon url="http://instagram.com/" bgColor="#d6249f" fgColor="#FFFFFF"style={{ height: 40, width: 40 }}/>
+                <SocialIcon url="http://linkedin.com/" bgColor="#0A66C2" fgColor="#FFFFFF"style={{ height: 40, width: 40 }}/>
+                <SocialIcon url="http://github.com/" bgColor="#333" fgColor="#FFFFFF" style={{ height: 40, width: 40 }} />
+            </ul>
+            <ul className="create">
                 <p>Create with &#x2764; by Anna Sharipkin</p>
-                </ul>
+            </ul>
             
         </footer>
      
