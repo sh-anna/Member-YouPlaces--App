8e1367d90e6256b0059c726203fc90bb6c3ee518
sh-anna <sharipkinanna@gmail.com>
2021-02-09 14:05:03 +0200

    clen code

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,4 +1,3 @@
-// const uuid = require('uuid/v4');
 const fs = require('fs');
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
@@ -47,7 +46,7 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  // if (!places || places.length === 0) {
+  
   if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError('Could not find places for the provided user id.', 404)
--- frontend/src/shared/components/uiElements/card/card.js
+++ frontend/src/shared/components/uiElements/card/card.js
@@ -1,9 +1,5 @@
 import React from 'react';
-// import { Link } from "react-router-dom";
-// import Avatar from "../avatar/avatar";
-// import "../../../../user/components/userItem/userItem.css";
 import './card.css';
-
 
 const Card = props => {
   return (
